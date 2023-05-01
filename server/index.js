538be4b943818f2f7be2f4101f461a34c12c09ca
Asuka <yalingliu8888@gmail.com>
2023-04-30 22:49:47 -0700

    add mongodb connection

--- server/index.js
+++ server/index.js
@@ -1,12 +1,26 @@
+require('dotenv').config();
+
 const express = require('express');
+const mongoose = require('mongoose');
+const Dish = require('./Dish');
 const app = express();
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB Connected...'))
+    .catch(err => console.log(err));
 
 app.get('/', (req, res) => {
     res.send('Home Page');
 });
 
-app.get('/menu', (req, res) => {
-    res.send('Menu Page');
+app.get('/menu', async (req, res) => {
+    try {
+        const dishes = await Dish.find({ available: true }); // Only return dishes that are available
+        res.json(dishes); // Send the dishes as a JSON response
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
 });
 
 app.get('/about', (req, res) => {
