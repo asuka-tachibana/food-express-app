3c444f5a48e37d43efa8bad3bdb2f927a65d8e86
Asuka <yalingliu8888@gmail.com>
2023-05-01 21:21:09 -0700

    update contact schema

--- server/Contact.js
+++ server/Contact.js
@@ -1,3 +1,5 @@
+const mongoose = require('mongoose');
+
 const ContactSchema = new mongoose.Schema({
     name: {
         type: String,
@@ -13,4 +15,4 @@ const ContactSchema = new mongoose.Schema({
     },
 });
 
-const Contact = mongoose.model('Contact', ContactSchema);
+module.exports = mongoose.model('Contact', ContactSchema);
--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const Dish = require('./Dish');
+const Contact = require('./Contact');
+
 const app = express();
 
 app.use(express.json());
