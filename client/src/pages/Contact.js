a81b732ff17f12f625e37772f03c191589000582
Asuka <yalingliu8888@gmail.com>
2023-05-03 21:49:09 -0700

    update contact page

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -1,23 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/Contact.css";
 
 function Contact() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await fetch(`${process.env.REACT_APP_API_BASE_URL}/contact`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ name, email, message }),
+    });
+
+    if (response.ok) {
+      alert("Message sent successfully!");
+      setName("");
+      setEmail("");
+      setMessage("");
+    } else {
+      alert("Error sending message. Please try again.");
+    }
+  };
+
   return (
     <div className="contact">
       <div className="rightSide">
         <h1> Contact Us</h1>
 
-        <form id="contact-form" method="POST">
+        <form id="contact-form" method="POST" onSubmit={handleSubmit}>
           <label htmlFor="name">Full Name</label>
-          <input name="name" placeholder="Enter full name..." type="text" />
+          <input
+            name="name"
+            placeholder="Enter full name..."
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
           <label htmlFor="email">Email</label>
-          <input name="email" placeholder="Enter email..." type="email" />
+          <input
+            name="email"
+            placeholder="Enter email..."
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
           <label htmlFor="message">Message</label>
           <textarea
             rows="6"
             placeholder="Enter message..."
             name="message"
             required
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
           ></textarea>
           <button type="submit"> Send Message</button>
         </form>
